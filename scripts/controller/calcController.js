b2fb33ca26ea1dac643d52d904f2b7cc3c159fba
Vandinho <evandro.menshaid@gmail.com>
2026-04-28 11:53:42 -0300

    C14 - Clicando mais de uma vez no botão igual

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,10 @@
 class CalcController {
 
     constructor(){
+
+        this._lastOperator = '';
+        this._lastNumber = '';
+
         this._operation = [];
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display");
@@ -66,15 +70,33 @@ class CalcController {
         }
     }
 
+    getResult(){
+
+
+        return eval(this._operation.join(""));
+    }
+
     calc(){
 
         let last = '';
 
+        this._lastOperator = this.getLastItem();
+
+        if(this._operation.length < 3){
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+        }
+
         if(this._operation.length > 3) {
-            let last = this._operation.pop();
+
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        }else if(this._operation.length == 3){
+                this._lastNumber = this.getLastItem(false);
         }
         
-        let result = eval(this._operation.join(""));
+        let result = this.getResult();
 
         if(last == '%'){
 
@@ -92,15 +114,27 @@ class CalcController {
 
     }
 
-    setLastNumberToDisplay(){
-        let lastNumber;
+    getLastItem(isOperator = true){
+        let lastItem;
 
         for(let i = this._operation.length-1; i >= 0; i--){
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i];
+
+            if(this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i];
                 break;
             }
+
         }
+
+        if(!lastItem){
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
+
+        return lastItem
+    }
+
+    setLastNumberToDisplay(){
+        let lastNumber = this.getLastItem (false);
 
         if(!lastNumber ) lastNumber = 0;
         this.displayCalc = lastNumber;
