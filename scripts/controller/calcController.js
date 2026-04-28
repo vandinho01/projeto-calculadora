4469a4540f5fab94735e510596d165a91f63895f
Vandinho <evandro.menshaid@gmail.com>
2026-04-28 10:47:45 -0300

    C13 - Botão Porcento

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -20,6 +20,8 @@ class CalcController {
 
         }, 1000);
 
+        this.setLastNumberToDisplay();
+
     }
 
     addEventListenerAll(element, events, fn){
@@ -33,11 +35,13 @@ class CalcController {
     }
 
     clearAll(){
-        this._operation = []
+        this._operation = [];
+        this.setLastNumberToDisplay();
     }
 
     clearEntry(){
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
     getLastOperation(){
@@ -63,11 +67,26 @@ class CalcController {
     }
 
     calc(){
-        let last = this._operation.pop();
 
+        let last = '';
+
+        if(this._operation.length > 3) {
+            let last = this._operation.pop();
+        }
+        
         let result = eval(this._operation.join(""));
 
-        this._operation = [result, last];
+        if(last == '%'){
+
+            result /= 100;
+
+            this._operation = [result];
+
+        }  else{
+            this._operation = [result];
+
+            if (last) this._operation.push(last);
+        }
 
         this.setLastNumberToDisplay();
 
@@ -82,6 +101,8 @@ class CalcController {
                 break;
             }
         }
+
+        if(!lastNumber ) lastNumber = 0;
         this.displayCalc = lastNumber;
     }
 
@@ -148,7 +169,7 @@ class CalcController {
                 this.addOperation('%')
                 break;
             case 'igual':
-
+                this.calc()
                 break;
             case 'ponto':
                 this.addOperation('.')
